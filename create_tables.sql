9c7d1584c98b8a6af8318d196bca62f2eba975da
sudhirk400 <sudhir.trivium@gmail.com>
2025-01-24 13:54:19 +0530

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,1 +1,54 @@
+CREATE TABLE Books (
+    bookID INT PRIMARY KEY,
+    AuthorID INT,
+    PublisherID INT,
+    Title TEXT,
+    ISBN TEXT,
+    Genre TEXT,
+    Type TEXT,
+    PublicationYear INT,
+    Price NUMERIC(10, 2),
+    Condition TEXT
+);
+
+CREATE TABLE Publishers (
+    publisherID INT PRIMARY KEY,
+    Country TEXT,
+    bookID INT REFERENCES Books(bookID)
+);
+
+CREATE TABLE Authors (
+    AuthorID INT PRIMARY KEY,
+    firstName TEXT,
+    lastName TEXT,
+    bookID INT REFERENCES Books(bookID)
+);
+
+CREATE TABLE Inventory (
+    bookID INT PRIMARY KEY REFERENCES Books(bookID),
+    stockLevelUsed INT,
+    stockLevelNew INT
+);
+
+CREATE TABLE Customers (
+    customerID INT PRIMARY KEY,
+    firstName TEXT,
+    lastName TEXT,
+    streetNumber INT,
+    streetName TEXT,
+    postalCode TEXT,
+    State TEXT,
+    Country TEXT,
+    phoneNumber TEXT
+);
+
+CREATE TABLE Orders (
+    orderID INT PRIMARY KEY,
+    customerID INT REFERENCES Customers(customerID),
+    orderDate DATE,
+    Subtotal NUMERIC(10, 2),
+    Shipping NUMERIC(10, 2),
+    Total NUMERIC(10, 2)
+);
+ 
 
